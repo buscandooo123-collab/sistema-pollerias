e1626b3d2a98554fb5939221c745f97163956e41
buscandooo123-collab <buscandooo123@gmail.com>
2026-01-27 11:02:26 -0600

    Sistema completo

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'poultry-system-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/madre/',
+        '/regreso/'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
